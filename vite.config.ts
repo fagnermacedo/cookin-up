5a8fecc890408476a48fc13a2572a3115cf44442
Fagner Sá <fagnermacedo@yahoo.com.br>
2024-06-30 00:16:19 -0300

    Resolvendo o problema de DNS, para obter a lista via JSON, site externo. Corrigindo VITE

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+//Resolvendo o problema de DNS, para obter a lista via JSON, site externo.
+import dns from 'dns'
+
+dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 
